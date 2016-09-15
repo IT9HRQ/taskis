1553cc952385b15bff25a025a47712bbd52dd511
Francesco Bianco <info.francescobianco@gmail.com>
2016-09-15 13:16:42 +0200

    start project

--- theme/footer.php
+++ theme/footer.php
@@ -1,5 +1,4 @@
-<script type="text/javascript">ar CLIENT_ID = '<?=file_get_contetns('../CLIENT_ID')?>';</script>
-
+<script type="text/javascript">var CLIENT_ID = '<?=file_get_contents('CLIENT_ID')?>';</script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/2.27.1/js/uikit.min.js"></script>
 <script src="js/client.js"></script>
